43151f8bac99c0c202bd08ef3b8a0bd50738dae1
waelhachani <med.wael.hachani@gmail.com>
2020-04-20 19:48:27 -0400

    Object Validation

--- ppmtool/src/main/java/com/wael/ppmtool/domain/Project.java
+++ ppmtool/src/main/java/com/wael/ppmtool/domain/Project.java
@@ -2,25 +2,38 @@ package com.wael.ppmtool.domain;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 public class Project {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@NotBlank(message ="Project name is required")
 	private String projectName;
+	@NotBlank(message ="Project Identifier is required")
+	@Size(min=4, max=5, message = "Please use 4 to 5 characters")
+	@Column(updatable = false, unique = true)
 	private String projectIdentifier;
+	@NotBlank(message ="Project Description is required")
 	private String Description;
+	@JsonFormat (pattern = "yyyy-mm-dd")
 	private Date start_Date;
+	@JsonFormat (pattern = "yyyy-mm-dd")
 	private Date end_Date;
-	
+	@JsonFormat (pattern = "yyyy-mm-dd")
 	private Date  created_At;
+	@JsonFormat (pattern = "yyyy-mm-dd")
 	private Date updated_At;
 	@PrePersist
 	protected void onCreate() {
