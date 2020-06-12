3f930fadceeed2879119cb7bf082cb5434bb9355
waelhachani <med.wael.hachani@gmail.com>
2020-06-12 10:11:39 -0400

    Object Validation Part2

--- ppmtool/src/main/java/com/wael/ppmtool/ProjectController.java
+++ ppmtool/src/main/java/com/wael/ppmtool/ProjectController.java
@@ -1,12 +1,19 @@
 package com.wael.ppmtool;
 
+
+
 import javax.validation.Valid;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.wael.ppmtool.domain.Project;
 import com.wael.ppmtool.services.ProjectService;
+
+
 
 @RestController
 @RequestMapping("/api/project")
@@ -26,8 +35,17 @@ public class ProjectController {
 	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
 		
 		if (result.hasErrors()) {
-			return new ResponseEntity<String>( "Invalid Project Object" , HttpStatus.BAD_REQUEST );
+			
+			Map<String, String> errorMap = new HashMap<String, String>();
+			
+			for (FieldError error  : result.getFieldErrors())
+				errorMap.put(error.getField(), error.getDefaultMessage());
+				
+			
+			return new ResponseEntity<Map<String, String> >( errorMap, HttpStatus.BAD_REQUEST );
 		}
+		
+		
 		
 		Project project1 = projectService.saveOrUpdateProject(project);
 		return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
